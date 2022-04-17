df9ec6214300d34fd6bede4a841cd91fbdc3fa65
annakochigina <kociginaanna@gmail.com>
2022-04-17 19:25:37 +0300

    first commit

--- src/Async.ts
+++ src/Async.ts
@@ -1,8 +1,15 @@
+import { resolve } from 'dns';
+
 /*
 Создайте функцию mock, которая принимает на вход аргумент number (количество миллисекунд) и возвращает Promise,
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
 }
 
 /*
@@ -11,19 +18,10 @@ export function mock(ms: number): Promise<number> {
 export function getData(): Promise<number[]> {
     const result: number[] = [];
 
-    return mock(100)
-        .then((data1) => {
-            result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
-            result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
-            result.push(data3);
-            return result;
-        });
+    return Promise.all([mock(100), mock(200), mock(300)]).then((data) => {
+        result.push(...data);
+        return result;
+    });
 }
 
 /*
@@ -32,8 +30,8 @@ export function getData(): Promise<number[]> {
  */
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
+        await Promise.reject(new Error('my error'));
     } catch (err) {
-        return err.message;
+        if (err instanceof Error) return err.message;
     }
 }
